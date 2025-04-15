e7b46c3026a633db7b6ed21f113ce21764f61b65
haritsimba <haritsimbafranck@gmail.com>
2025-04-15 09:47:59 +0300

    modif : dashboard

--- app/Livewire/Tiers/ContactsDashboard.php
+++ app/Livewire/Tiers/ContactsDashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Tiers;
 
+use App\Services\ContactService;
 use Livewire\Component;
 use Exception;
 use Illuminate\Http\Request;
@@ -12,6 +13,59 @@ use Illuminate\Support\Facades\Auth;
 
 class ContactsDashboard extends Component
 {
+    public $contacts = [];
+    public $proposals = [];
+    public function mount(ContactService $contactsService)
+    {
+        $this->contacts = json_decode(json_encode($contactsService->getAllContacts(limit: 3), true));
+        $this->getAllProposals();
+    }
+
+    private function getAllProposals()
+    {
+        try {
+            $user = Auth::user();
+            $getProposalsResponse = Http::withHeaders([
+                'DOLAPIKEY' => $user->api_key,
+                'Accept' => 'application/json'
+            ])->get($user->url_dolibarr . '/api/index.php/proposals?sortfield=t.rowid&limit=10&sortorder=DESC');
+
+            if ($getProposalsResponse->successful()) {
+
+
+                $this->proposals = collect($getProposalsResponse->json())->map(function ($item) use ($user) {
+                    $item = (object) $item;
+                    $proposal = (object) [
+                        'socid' => $item->socid,
+                        'ref' => $item->ref,
+                        'contacts_ids' => $item->contacts_ids,
+                        'datep' => $item->datep,
+                        'datep_string' => date('d/m/Y', $item->datep),
+                        'duree_validite' => $item->duree_validite,
+                        'cond_reglement_id' => $item->cond_reglement_id,
+                        'mode_reglement_id' => $item->mode_reglement_id,
+                        'demand_reason_id' => $item->demand_reason_id,
+                        'availability_id' => $item->availability_id,
+                        'fin_validite' => $item->fin_validite,
+                        'date_fin_validite_string' => date('d/m/Y', $item->fin_validite),
+                        'user_author' => $item->user_author,
+                        'total_ht' => $item->total_ht,
+                        'client_categories' => [],
+                        'client' => (object) ['display_name' => ""],
+                        'user_author_id' => $item->user_author_id,
+                        'user_author_display_name' => '',
+                        'user_author_name' => '',
+                        'lines' => []
+                    ];
+
+                    return $proposal;
+                })->all();
+            }
+        } catch (Exception $e) {
+            dump($e->getMessage());
+        }
+    }
+
     public function render()
     {
         $user  = Auth::user();
